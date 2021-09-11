05b80c3769f10b1a5858fc46c7305778d82aa6b6
qusba <kasper.kahkonen@outlook.com>
2021-09-11 12:27:56 +0300

    täydennettiin määrittelydokumenttia

--- Dokumentaatio/maarittelydokumentti.mk
+++ Dokumentaatio/maarittelydokumentti.mk
@@ -1,1 +1,17 @@
-#Määrittelydokumentti
+# Määrittelydokumentti
+
+## Projekti
+
+Projektin tavoite on toteuttaa Huffmannin ja Lempel-Zivin pakkausalgoritmit käyttäen Python-ohjelmointikieltä.
+Algoritmien toteuttamisen jälkeen on tarkoituksena vertailla niiden suorituskykyä eri syötteillä. Toivottava tehokkuus tekstiä pakatessa on 40-60% tiedoston alkuperäisestä koosta.
+
+Valitsin aiheeksi tiedonpakkaamisen, sillä se ei ole minulle entuudestaan tuttua. Huffmanin ja Lempel-Zivin algoritmit oli mainittu kurssin materiaalissa, joten päätin ottaa ne projektiin mukaan.
+Tavoitteenani on että syötteenä voi toimia mikä tahansa tiedosto ja lopputuloksena on muttuumaton pienempi tiedosto. Muuttumattomilla menetelmillä ei kuitenkaan ole mielekästä pakata kaikenlaista tiedoa, joten testaaminen tulee painottumaan pääosin tekstitiedostoihin.
+
+## Projektin kielet
+
+Projektin ohjelmointikielenä toimii Python. En tällä hetkellä hallitse muita ohjelmointikieliä. Koodin ja dokumentaation kielenä toimii suomi. Hallitsen hyvin englantia ja kykenen vertaisarvioimaan englanninkielellä toteutettuja projekteja.
+
+## Opinto-ohjelma
+
+Opiskelen tietojenkäsittelytieteen kandidaatin tutkintoa varten.
