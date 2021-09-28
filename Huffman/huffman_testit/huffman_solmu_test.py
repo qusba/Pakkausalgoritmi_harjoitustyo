c47a4442edc25667bf9a138b6bda1edd4ff02dee
qusba <kasper.kahkonen@outlook.com>
2021-09-28 18:53:29 +0300

    luotiin testikansio

---
+++ Huffman/huffman_testit/huffman_solmu_test.py
@@ -0,0 +1,14 @@
+import unittest
+from Huffman.huffman_solmu import HuffmanSolmu
+
+class TestHuffmanSolmu(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+def test_solmu_luodaan_oikein(self):
+    solmu = HuffmanSolmu("f",30)
+    self.assertEqual(solmu.merkki,"f")
+    self.assertEqual(solmu.ilmaantuvuus,30)
+    self.assertEqual(solmu.vasen_lapsi,None)
+    self.assertEqual(solmu.oikea_lapsi,None)
