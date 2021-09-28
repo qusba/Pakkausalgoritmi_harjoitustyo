439cbfa8bff44ba51d7da6e23cfcade0910fc869
qusba <kasper.kahkonen@outlook.com>
2021-09-28 20:49:21 +0300

    luotiin HuffmanSolmulle yksikkötestit

---
+++ Huffman/huffman_testit/huffman_koodaus_test.py
@@ -0,0 +1,7 @@
+import unittest
+from huffman_koodaus import HuffmanKoodaus
+
+class TestHuffmanKoodaus(unittest.TestCase):
+    def setUp(self):
+        self.input_polku = "/home/kasperka/testi.txt"
+        
--- Huffman/huffman_testit/huffman_solmu_test.py
+++ Huffman/huffman_testit/huffman_solmu_test.py
@@ -6,9 +6,18 @@ class TestHuffmanSolmu(unittest.TestCase):
         pass
 
 
-def test_solmu_luodaan_oikein(self):
-    solmu = HuffmanSolmu("f",30)
-    self.assertEqual(solmu.merkki,"f")
-    self.assertEqual(solmu.ilmaantuvuus,30)
-    self.assertEqual(solmu.vasen_lapsi,None)
-    self.assertEqual(solmu.oikea_lapsi,None)
+    def test_solmu_luodaan_oikein(self):
+        solmu = HuffmanSolmu("f",30)
+        self.assertEqual(solmu.merkki,"f")
+        self.assertEqual(solmu.ilmaantuvuus,30)
+        self.assertEqual(solmu.vasen_lapsi,None)
+        self.assertEqual(solmu.oikea_lapsi,None)
+        self.assertEqual(solmu.bittikoodi,"")
+
+    def test_solmulle_voidaan_osoittaa_vanhemmat(self):
+        solmu1 = HuffmanSolmu("f",30)
+        solmu2 = HuffmanSolmu("d",50)
+        solmu3 = HuffmanSolmu(None,solmu1.ilmaantuvuus+solmu2.ilmaantuvuus,solmu1,solmu2)
+
+        self.assertEqual(solmu3.vasen_lapsi,solmu1)
+        self.assertEqual(solmu3.oikea_lapsi,solmu2)
